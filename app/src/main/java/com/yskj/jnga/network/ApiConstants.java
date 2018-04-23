7c7d19ff173246cd43dd93b0bb14a47485dd168b
saberccmiku <639878266@qq.com>
2018-04-23 17:32:49 +0800

    参数

--- app/src/main/java/com/yskj/jnga/network/ApiConstants.java
+++ app/src/main/java/com/yskj/jnga/network/ApiConstants.java
@@ -12,8 +12,6 @@ public class ApiConstants {
     // 登录系统
     public static String USERID = "docadmin";
     public static String PSWID = "passw0rd";
-
-//    public static final String CONNIP = "219.159.71.168:9080";// lmc本地测试用服务器
     public static final String CONNIP = "192.168.0.106:9080";//lmc本地测试用服务器
     public static final String FILE_PATH = "/IDOC/service/file/";
     public static final String PATH = "/IDOC/WebService";// 服务器路径
